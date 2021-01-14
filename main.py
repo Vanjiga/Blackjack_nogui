cab2ece218838a5a3834a3590c9d5913160fc972
Vanjiga <vanja.gacic123@gmal.com>
2021-01-15 00:15:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 import random
 import itertools
 product=()
+hand=[]
 cardvalue = ["1","2","3","4","5","6","7","8","9","10","Ace","Jack","Queen","King"]
 cardsuit = ["Of diamonds", "Of clubs", "Of spades", "Of hearts"]
 
 deck = list(itertools.product(cardvalue, cardsuit))
 random.shuffle(deck)
 
-print(random.choice(deck))
+hand=(random.choice(deck))
+print(hand)
