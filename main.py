d6b0c65194d1fa99b16e750bbbded0be3345f93e
Vanjiga <vanja.gacic123@gmal.com>
2021-01-14 18:41:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hellow")
+import random
+import itertools
+product=()
+cardvalue = ["1","2","3","4","5","6","7","8","9","10","Ace","Jack","Queen","King"]
+cardsuit = ["Of diamonds", "Of clubs", "Of spades", "Of hearts"]
+
+deck = list(itertools.product(cardvalue, cardsuit))
+random.shuffle(deck)
+
+print(random.choice(deck)
