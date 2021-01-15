808f85f3df1dc2ffab235bbca6e80bb7fea65fc7
Vanjiga <vanja.gacic123@gmal.com>
2021-01-15 13:15:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-
+#imports
 import random
 import itertools
-product=()
+
+#global varriales 
 hand=[]
-cardvalue = ["1","2","3","4","5","6","7","8","9","10","Ace","Jack","Queen","King"]
-cardsuit = ["Of diamonds", "Of clubs", "Of spades", "Of hearts"]
+dealer=[]
+starter_hand = 2
+i = 0
 
-deck = list(itertools.product(cardvalue, cardsuit))
-random.shuffle(deck)
+#starter logic
+g=int(input("new game? 1 yes 0 no "))
+if g > 0:
+    #card deck
+    print("game")
+    cardvalue = ["2","3","4","5","6","7","8","9","10","Ace","Jack","Queen","King"]
+    cardsuit = ["Of diamonds", "Of clubs", "Of spades", "Of hearts"]
 
-hand=(random.choice(deck))
-print(hand)
+    deck = list(itertools.product(cardvalue, cardsuit))
+    random.shuffle(deck)
+    #output
+    x = (random.choice(deck))
+    for i in starter_hand:
+        hand.append(x)
+        print(hand)
+
+else:
+    print("no game")
+
